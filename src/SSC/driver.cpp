394de92a0836aa9e80cdfcae769fa96b2c3fc4d6
Gabriel Castro <gabriel1997.castro@gmail.com>
2018-02-16 14:25:27 -0400

    update comments

--- src/SSC/driver.cpp
+++ src/SSC/driver.cpp
@@ -1,7 +1,7 @@
 /* Universidade de Brasília
  * Laboratório de Automação e Robótica
  * Autor: Gabriel Guimarães Almeida de Castro
- * Programa: Básico da comunicação serial
+ * Programa: Básico da comunicação serial e movimentação dos servos
  */
 
 #include <stdio.h>   /* Standard input/output definitions */
@@ -15,8 +15,6 @@
 #include <stdlib.h>
 #include <sstream> 
 int fd;
-
-int servoPW[32] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
 
 //Convert integer to string
 std::string int_to_string(int n)
@@ -55,24 +53,25 @@ void moveServo(int ch, int pos)
 
 //centralizes all servos
 void setServos(int num) {
-    int NUM_ACTIVE_SERVOS = num;
-    for(int i = 1; i <= NUM_ACTIVE_SERVOS; i++) {
-        moveServo(i, 1500);
-        sleep(0.1); //give each servo initial wait time
-   }
+  int NUM_ACTIVE_SERVOS = num;
+
+  for(int i = 1; i <= NUM_ACTIVE_SERVOS; i++) {
+    moveServo(i, 1500);
+    sleep(0.1); //give each servo initial wait time
+  }
 }
 
 int main()
 {
-        int fd1 = open_port();
-        fd = fd1;
-        setServos(6);
-        moveServo(5,500);
-        sleep(1);
-        moveServo(5,1500);
-        moveServo(4,2500);
-        sleep(1);
-        moveServo(4,500);
-        close(fd);
+  int fd1 = open_port();
+  fd = fd1;
+  setServos(6);
+  moveServo(5,500);
+  sleep(1);
+  moveServo(5,1500);
+  moveServo(4,2500);
+  sleep(1);
+  moveServo(4,500);
+  close(fd);
 
 }
