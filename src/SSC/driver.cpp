bb2895d7ed985293f4bb220f6b5db06fb9868781
Gabriel Castro <gabriel1997.castro@gmail.com>
2018-02-16 14:18:48 -0400

    add setServos and moveServo function

--- src/SSC/driver.cpp
+++ src/SSC/driver.cpp
@@ -10,16 +10,26 @@
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
+#include <iostream>
+#include <string> 
+#include <stdlib.h>
+#include <sstream> 
+int fd;
 
-/*
- * 'open_port()' - Open serial port 1.
- *
- * Returns the file descriptor on success or -1 on error.
- */
+int servoPW[32] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
 
-int open_port(void) {
+//Convert integer to string
+std::string int_to_string(int n)
+{
+  std::ostringstream str1;
+  str1 << n;
+  std::string str = str1.str();
+  return str1.str();
+}
+
+int open_port(void)
+{
   int fd; /* File descriptor for the port */
-
 
   fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
   if (fd == -1)
@@ -32,12 +42,37 @@ int open_port(void) {
   return (fd);
 }
 
-int main(){
-        int fd = open_port();
-        int n = write(fd, "#5 P600\r", 9); //
+//move the ch servo to postition pos
+void moveServo(int ch, int pos)
+{
+  int n;
+  std::string str = '#' + int_to_string(ch) + " P" + int_to_string(pos) + '\r';
+  const char * msg = str.c_str();
+  n = write(fd, msg, strlen(msg));
+  if (n < 0)
+  fputs("write() failed!\n", stderr);
+}
+
+//centralizes all servos
+void setServos(int num) {
+    int NUM_ACTIVE_SERVOS = num;
+    for(int i = 1; i <= NUM_ACTIVE_SERVOS; i++) {
+        moveServo(i, 1500);
+        sleep(0.1); //give each servo initial wait time
+   }
+}
+
+int main()
+{
+        int fd1 = open_port();
+        fd = fd1;
+        setServos(6);
+        moveServo(5,500);
         sleep(1);
-        n = write(fd, "#5 P2400\r", 10);
-        if (n < 0)
-                fputs("write() of 4 bytes failed!\n", stderr);
+        moveServo(5,1500);
+        moveServo(4,2500);
+        sleep(1);
+        moveServo(4,500);
         close(fd);
+
 }
