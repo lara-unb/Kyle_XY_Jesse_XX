bf3e828cff311ccbfe90ea67893b2e9dcde9be9e
Gabriel Castro <gabriel1997.castro@gmail.com>
2018-02-19 19:46:57 -0400

    add verifyBounds function

--- src/SSC/driver.cpp
+++ src/SSC/driver.cpp
@@ -13,8 +13,14 @@
 #include <iostream>
 #include <string> 
 #include <stdlib.h>
-#include <sstream> 
+#include <sstream>
+
+
 int fd;
+int servoMinPos = 500;
+int servoMaxPos = 2500;
+int servoMinCh = 0;
+int servoMaxCh = 31;
 
 //Convert integer to string
 std::string int_to_string(int n)
@@ -25,7 +31,22 @@ std::string int_to_string(int n)
   return str1.str();
 }
 
-int open_port(void)
+int verifyBounds(int ch, int pos)
+{
+  if (ch > servoMaxCh or ch < servoMinCh)
+  {
+    std::cout << "Channel is not within its boundaries\n";
+    return 0;
+  }
+  if (pos > servoMaxPos or pos < servoMinPos)
+  {
+    std::cout << "Position is not within its boundaries\n";
+    return 0;
+  }
+  return 1;
+}
+
+int openPort(void)
 {
   int fd; /* File descriptor for the port */
   struct termios options;
@@ -34,7 +55,7 @@ int open_port(void)
   fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
   if (fd < 0)
   {
-    perror("open_port: Unable to open /dev/ttyUSB0\n"); //It coud not open port
+    perror("openPort: Unable to open /dev/ttyUSB0\n"); //It coud not open port
   }
   else
     fcntl(fd, F_SETFL, 0);
@@ -62,38 +83,45 @@ int open_port(void)
   return (fd);
 }
 
-//move the ch servo to postition pos
+// Move the ch servo to postition pos
 void moveServo(int ch, int pos)
 {
-  int n;
-  std::string str = '#' + int_to_string(ch) + " P" + int_to_string(pos) + '\r';
-  const char * msg = str.c_str();
-  n = write(fd, msg, strlen(msg));
-  if (n < 0)
-  fputs("write() failed!\n", stderr);
+  if(verifyBounds(ch, pos) == 1)
+  {
+    int n;
+    std::string str = '#' + int_to_string(ch) + " P" + int_to_string(pos) + '\r';
+    const char * msg = str.c_str();
+    n = write(fd, msg, strlen(msg));
+    if (n < 0)
+      fputs("write() failed!\n", stderr);
+  }
 }
 
-//centralizes all servos
+// Centralizes all servos
 void setServos(int num) {
   int NUM_ACTIVE_SERVOS = num;
 
   for(int i = 1; i <= NUM_ACTIVE_SERVOS; i++) {
-    moveServo(i, 1500);
+    moveServo(i-1, 1500);
     sleep(0.1); //give each servo initial wait time
   }
 }
 
 int main()
 {
-  int fd1 = open_port();
+  int fd1 = openPort();
   fd = fd1;
   setServos(6);
+  sleep(1);
   moveServo(5,500);
   sleep(1);
-  moveServo(5,1500);
-  moveServo(4,2500);
+  moveServo(5,2500);
+  //moveServo(4,2500);
+ // moveServo(1,2000);
   sleep(1);
   moveServo(4,500);
+  moveServo(0,2500);
+  
   close(fd);
 
 }
