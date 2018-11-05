2de718dc2c9a7f7bd75aa6f30272555093fe171b
Felps Sousa <felipefelpsrs@gmail.com>
2018-11-05 01:00:13 -0200

    Adicionando biblioteca ERROR

---
+++ kj_arms/include/kj_arms/error_codes_kj_arms.h
@@ -0,0 +1,19 @@
+/***********************************************************************************************************
+*** Arquivo: error_codes_kj_arms.h
+*** Conteudo: Definições para ERROR
+*** Modificado por: Felipe Luis Rodrigues Sousa
+*** Código fonte: https://github.com/mnovo/lynxmotion_ros/blob/master/include/lynxmotion_ssc32/error_codes.h
+************************************************************************************************************/
+
+#define SSC32_INVALIDE_HANDLE			-1
+#define SSC32_SECESS					0
+#define SSC32_ERROR_OPEN_PORT			1
+#define SSC32_ERROR_NOT_CONNECTED		2
+#define SSC32_ERROR_TRANSMIT_FAILED		3
+#define SSC32_ERROR_SERIAL_IO			4
+#define SSC32_ERROR_SERIAL_RECEIVE		5
+#define SSC32_CHANNELS_OUT_RANGE		6
+#define SSC32_CH_OUT_RANGE 				7
+#define SSC32_PW_OUT_RANGE				8
+#define SSC32_SPD_OUT_RANGE				9
+#define SSC32_BANK_OUT_RANGE			10
