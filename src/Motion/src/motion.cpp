e762b70960c549b23e8f4d917b588bb413be2675
Gabriel Castro <gabriel1997.castro@gmail.com>
2019-04-11 15:52:06 -0300

    Improve motion code

--- src/Motion/src/motion.cpp
+++ src/Motion/src/motion.cpp
@@ -27,12 +27,22 @@
 #include "SSC.h"
 
 // Definicoes internas:
-#define MAIN_MODULE_INIT(cmd_init) 	if(cmd_init==0){printf("    Erro em %s",#cmd_init);return(0);}
-#define MAIN_MODULE_CLOSE(cmd_close) 	if(cmd_close==0){printf("    Erro em %s",#cmd_close);}
+#define MAIN_MODULE_INIT(cmd_init)           \
+	if (cmd_init == 0)                       \
+	{                                        \
+		printf("    Erro em %s", #cmd_init); \
+		return (0);                          \
+	}
+#define MAIN_MODULE_CLOSE(cmd_close)          \
+	if (cmd_close == 0)                       \
+	{                                         \
+		printf("    Erro em %s", #cmd_close); \
+	}
 
 // Definições para usar tic e toc pra cálculos de tempo
 //---------------------------------------------------------------------------------------------------------
-struct{
+struct
+{
 	struct timeval time;
 	struct timeval timereset;
 } tictocctrl;
@@ -45,37 +55,45 @@ void tic(void)
 double toc(void)
 {
 	gettimeofday(&tictocctrl.time, NULL);
-	return ((tictocctrl.time.tv_sec - tictocctrl.timereset.tv_sec) + (tictocctrl.time.tv_usec - tictocctrl.timereset.tv_usec)*1e-6);
+	return ((tictocctrl.time.tv_sec - tictocctrl.timereset.tv_sec) + (tictocctrl.time.tv_usec - tictocctrl.timereset.tv_usec) * 1e-6);
 }
 //---------------------------------------------------------------------------------------------------------
 
-void catch_signal(int sig)
+void signalHandler(int signum)
 {
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+
+    printf("\n*** Encerrando o modulo sensoray526...");
+    MAIN_MODULE_CLOSE(sensoray526_close());
+    printf("\n\n");
+    fflush(stdout); // mostra todos printfs pendentes.
+
+    exit(signum);
 }
 
 //Lê os encoder por mais ou menos 1s
 //---------------------------------------------------------------------------------------------------------
 int readEncoder(void)
 {
-    float texec;
-	unsigned char counter=0;
+	float texec;
+	unsigned char counter = 0;
 	//Configura encoder 0
 	sensoray526_configure_encoder(0);
 	sensoray526_configure_encoder(1);
 	sensoray526_reset_counter(0);
 	sensoray526_reset_counter(1);
-	tic(); 
-	for(counter=0;counter<10;counter++)
+	tic();
+	for (counter = 0; counter < 10; counter++)
 	{
 		//Le o encoder 0
-		printf("\n Encoder 0: (%ld)",sensoray526_read_counter(0));
-		printf("\n Encoder 1: (%ld)",sensoray526_read_counter(1));
-	
+		printf("\n Encoder 0: (%ld)", sensoray526_read_counter(0));
+		printf("\n Encoder 1: (%ld)", sensoray526_read_counter(1));
+
 		// Sleep
 		usleep(100000);
 	}
 	texec = toc(); //us
-    printf("\nRead encoder took %f seconds to execute \n", texec*1e6);
+	printf("\nRead encoder took %f seconds to execute \n", texec * 1e6);
 	return 1;
 }
 //---------------------------------------------------------------------------------------------------------
@@ -83,69 +101,65 @@ int readEncoder(void)
 // Calcula a velocidade das rodas em rad/s
 void computeVel(void)
 {
-    float texec;
-	unsigned char counter=0;
-    long n0 = 0;
-    long n1 = 0;
-    double w;
-    
+	float texec;
+	unsigned char counter = 0;
+	long n0 = 0;
+	long n1 = 0;
+	double w0, w1;
+
 	sensoray526_configure_encoder(0);
 	sensoray526_configure_encoder(1);
-	sensoray526_reset_counter(0);
-	sensoray526_reset_counter(1);
 
-	tic(); 
-    
-    // Sleep
-	usleep(100000);
-        
-    n0 = sensoray526_read_counter(0);  //n of pulses encoder 0
-    n1 = sensoray526_read_counter(1);  //n of pulses encoder 1
-    
-    texec = toc();
-    w = (2 * 3.14159275 * n0) / (100 * 30 * texec); 
-    printf("\n Speed: %f rad/s", w);
+	while (true)
+	{
+		sensoray526_reset_counter(0);
+		sensoray526_reset_counter(1);
+		tic();
+
+		// Sleep
+		usleep(100000);
+
+		n0 = sensoray526_read_counter(0); //n of pulses encoder 0
+		n1 = sensoray526_read_counter(1); //n of pulses encoder 1
+		texec = toc();
+		w0 = (0.0020943952 * n0) / texec; // (2 * pi) / (100 cycles * 30) = const = 0.0020943952
+        w1 = (0.0020943952 * n1) / texec;
+		printf("\n Speed1: %f rad/s           Speed2: %f rad/s", w0, w1);
+	}
 }
 //---------------------------------------------------------------------------------------------------------
-
 
 int main()
 {
 	int mode = 0;
-     std::string command;
+	std::string command;
 
-    signal(SIGTERM, catch_signal);
-	signal(SIGINT, catch_signal);
-    
-    //run 
-    command = "#0 P1700 #1 P1700";
-    sendCommand(command.c_str());
+	// Cadastra funções para encerrar o programa
+    signal(SIGTERM, signalHandler);
+    signal(SIGINT, signalHandler);
+
+	//run
+	command = "#0 P1700 #1 P1700";
+	sendCommand(command.c_str());
 
 	/* Avoids memory swapping for this program */
-	mlockall(MCL_CURRENT|MCL_FUTURE);
-	
+	mlockall(MCL_CURRENT | MCL_FUTURE);
+
 	// robot module:
 	printf("\n*** Iniciando o modulo sensoray526...");
 	MAIN_MODULE_INIT(sensoray526_init());
-	
-    computeVel();
-    computeVel();
-    computeVel();
-    sleep(10);
-    computeVel();
-	
-    readEncoder();
 
-    command = "#0 P1500 #1 P1500";
-    sendCommand(command.c_str());
-    sleep(1);
-    computeVel();
-    computeVel();
-    computeVel();
+	computeVel();
+
+
+	command = "#1 P1500 #1 P1500";
+	sendCommand(command.c_str());
+	sleep(1);
+	computeVel();
 	printf("\n*** Encerrando o modulo robot...");
 	MAIN_MODULE_CLOSE(sensoray526_close());
-	
+
 	printf("\n\n");
 	fflush(stdout); // mostra todos printfs pendentes.
-    return 1;
+	return 1;
 }
